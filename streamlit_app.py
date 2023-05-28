aba0e4d2e02c93e0228db4b313a35580973d798d
deepaksainics <64352811+deepaksainics@users.noreply.github.com>
2023-05-29 01:28:48 +0530

    Update streamlit_app.py
    Print different text sizes in stream lit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
 import streamlit
 
-streamlit.title('This is my first streamlit app')
+streamlit.title('This is my first streamlit app header')
+streamlit.header('Small header')
+streamlit.text('Normal text')
