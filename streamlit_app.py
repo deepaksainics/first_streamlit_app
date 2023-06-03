5482b88f08eba9f5380f3677ea11558db958ddd8
deepaksainics <64352811+deepaksainics@users.noreply.github.com>
2023-06-04 00:25:53 +0530

    Update streamlit_app.py
    Queried snowflake data

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,9 +46,16 @@ fruityvice_normalized_advice = pd.json_normalize(fruityvice_response_advice.json
 # Output the table
 streamlit.dataframe(fruityvice_normalized_advice)
 
+#printing hello Snowflake
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
+
+#Quering snowflake tables
+my_cur.execute("select * from fruit_load_list")
+my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_rows)
